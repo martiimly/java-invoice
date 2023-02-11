fdbb0f66b4f6f381cc3deed8f4949b04cab43bb1
student13 <student13@pcoip.ki.agh.edu.pl>
2023-02-11 10:34:13 +0100

    First four tests of ProductTest are green

--- src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
+++ src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
@@ -9,25 +9,26 @@ public abstract class Product {
 
     private final BigDecimal taxPercent;
 
-    protected Product(String name, BigDecimal price, BigDecimal tax) {
+    protected Product(String name, BigDecimal price, BigDecimal tax){
         this.name = name;
         this.price = price;
         this.taxPercent = tax;
     }
 
     public String getName() {
-        return null;
+        return this.name;
     }
 
     public BigDecimal getPrice() {
-        return null;
+        return this.price;
     }
 
     public BigDecimal getTaxPercent() {
-        return null;
+        return this.taxPercent;
     }
 
     public BigDecimal getPriceWithTax() {
-        return null;
+        BigDecimal priceWithTax = this.price.multiply(this.taxPercent).add(this.price);
+        return priceWithTax;
     }
 }
