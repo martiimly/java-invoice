9d48e32a0e34d1f170ab70a8800371837693ddc1
Marta Mlynska <martiimly@gmail.com>
2023-03-05 18:34:19 +0100

    Invoice is ready

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -8,26 +8,38 @@ import pl.edu.agh.mwo.invoice.product.Product;
 
 public class Invoice {
 //    private Collection<Product> products;
-    private Map<Product, Integer> products = new HashMap<>();
+    private Map<Product, Integer> products = new HashMap<Product, Integer>();
 
     public void addProduct(Product product) {
-        // TODO: implement
+        addProduct(product, 1);
     }
 
     public void addProduct(Product product, Integer quantity) {
-        // TODO: implement
+        if (product == null || quantity <= 0){
+            throw new IllegalArgumentException();
+        }
         this.products.put(product, quantity);
     }
 
     public BigDecimal getNetPrice() {
-        return BigDecimal.ZERO;
+        BigDecimal priceNet = BigDecimal.ZERO;
+        for (Product product: products.keySet()){
+            BigDecimal quantity = new BigDecimal(products.get(product));
+            priceNet = priceNet.add(product.getPrice().multiply(quantity));
+        }
+        return priceNet;
     }
 
     public BigDecimal getTax() {
-        return null;
+        return getTotal().subtract(getNetPrice());
     }
 
     public BigDecimal getTotal() {
-        return null;
+        BigDecimal total = BigDecimal.ZERO;
+        for (Product product: products.keySet()){
+            BigDecimal quantity = new BigDecimal(products.get(product));
+            total = total.add(product.getPriceWithTax().multiply(quantity));
+        }
+        return total;
     }
 }
